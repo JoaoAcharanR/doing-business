12ba2c6d860b374896f07e2ecd668ebe9a189d52
Joao Acharán Riffo <joaoacharan@gmail.com>
2020-05-17 18:30:13 -0400

    Incorporación 2017

--- code/code_04_correlations.R
+++ code/code_04_correlations.R
@@ -24,3 +24,22 @@ cor2016 <- bbdd_2016_2020 %>%
   chart.Correlation(histogram = TRUE,
                     pch = 19)
 
+
+cor2017 <- bbdd_2016_2020 %>%
+  filter(Year == 2017) %>%
+  select("Ease of doing business score (DB17-20 methodology)", "Score-Starting a business",
+         "Score-Dealing with construction permits (DB16-20 methodology)",
+         "Score-Getting electricity (DB16-20 methodology)",
+         "Score-Protecting minority investors (DB15-20 methodology)",
+         "Score-Trading across borders (DB16-20 methodology)",
+         "Score-Registering property (DB17-20 methodology)",
+         "Score-Getting credit (DB15-20 methodology)",
+         "Score-Paying taxes (DB17-20 methodology)",
+         "Score-Enforcing contracts (DB17-20 methodology)",
+         "Score-Resolving insolvency",
+         log_SI.POV.DDAY,
+         SI.DST.FRST.20,
+         SI.POV.GINI,
+         log_NY.GDP.PCAP.PP.KD) %>%
+  chart.Correlation(histogram = TRUE,
+                    pch = 19)
