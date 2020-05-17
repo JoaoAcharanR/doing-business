b1890165622b9958fdce74d3422df99ab8cb593b
Joao Acharán Riffo <joaoacharan@gmail.com>
2020-05-17 16:51:36 -0400

    Librerias a cargar

---
+++ code/code_01_setup_01_libraries.R
@@ -0,0 +1,14 @@
+#####CARGAR LIBRERIAS#####
+library(readxl)
+library(car)
+library(ggplot2)
+library(reshape)
+library(gridExtra)
+library(AER)
+library(memisc)
+library(lmtest)
+library(texreg)
+library(orcutt)
+library(tidyverse, warn = FALSE)
+
+getwd()
